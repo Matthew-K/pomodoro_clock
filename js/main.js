3490233c909142d11c7a822daa726af34599b175
Matthew Kolodziejczak <makolodzi@gmail.com>
2016-06-06 16:11:33 -0400

    Have break start after session is done, and vice versa

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ model = {
 controller = {
 
 	init: function(){
-		view.createClickHandlers();
+		view.init();
 	},
 
 	updateBreakLength: function(num){
@@ -51,12 +51,40 @@ controller = {
 
 view = {
 
+	init: function(){
+		view.createClickHandlers();
+	},
+
+	timer: function(minutes, seconds, breakOrSession){
+		$("#timer").text(minutes + " " + seconds);
+		if(minutes === 0 && seconds === 0){
+			var newTime = null;
+			if(breakOrSession === 'session'){
+				newTime = controller.getBreakLength();
+				$("#displayTimeHeader").text("Break");
+				// switch over to the break time
+				view.timer(newTime, 0, 'break');
+			} else if (breakOrSession === 'break'){
+				newTime = controller.getSessionLength(); 
+				$("#displayTimeHeader").text("Session");
+				// switch over to the session time
+				view.timer(newTime, 0, 'break');
+			}
+			return;
+		} else if(seconds === 0){
+			minutes --;
+			seconds = 60;
+		}
+		seconds --;
+		setTimeout('view.timer(' + minutes + ',' + seconds + ',"' + breakOrSession + '")',1000);
+	},
+
 	createClickHandlers: function(){
-		this.breakSubtract();
-		this.breakAdd();
-		this.sessionSubtract();
-		this.sessionAdd();
-		this.startButton();
+		view.breakSubtract();
+		view.breakAdd();
+		view.sessionSubtract();
+		view.sessionAdd();
+		view.startButton();
 	},
 
 	breakSubtract: function(){
@@ -118,23 +146,14 @@ view = {
 	startButton: function(){
 		$("#start").on("click", function(){
 			var time = controller.getTimer();
-			view.timer(time, 0);
+			if (time === 0){
+				return;
+			} else {
+				view.timer(time, 0, 'session');
+			}
 		});
 	},
 
-	timer: function(minutes, seconds){
-		$("#timer").text(minutes + " " + seconds);
-		if(minutes === 0 && seconds === 0){
-			$("#timer").text('Done!');
-			return;
-		} else if(seconds === 0){
-			minutes --;
-			seconds = 60;
-			setTimeout('timer(' + minutes + ',' + seconds + ')',1000);
-		}
-		seconds --;
-		setTimeout('timer(' + minutes + ',' + seconds + ')',1000);
-	}
 
 };
 
